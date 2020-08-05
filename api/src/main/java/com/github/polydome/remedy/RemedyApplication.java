3869fe10f6da3894e62f362f79181626d7631789
polydome <polydome@outlook.com>
2020-08-05 16:23:56 +0200

    Create Spring application

---
+++ api/src/main/java/com/github/polydome/remedy/RemedyApplication.java
@@ -0,0 +1,13 @@
+package com.github.polydome.remedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemedyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemedyApplication.class, args);
+    }
+
+}
