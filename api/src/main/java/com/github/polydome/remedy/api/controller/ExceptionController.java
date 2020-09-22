c5de89140e9bcc6a773178e5d93e7600cad59418
polydome <polydome@outlook.com>
2020-09-22 18:29:47 +0200

    Fix incorrect status when packaging was not found

--- api/src/main/java/com/github/polydome/remedy/api/controller/ExceptionController.java
+++ api/src/main/java/com/github/polydome/remedy/api/controller/ExceptionController.java
@@ -4,6 +4,7 @@ import com.github.polydome.remedy.api.model.ApiError;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,7 +16,8 @@ import javax.persistence.EntityNotFoundException;
 public class ExceptionController {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ApiError entityNotExists(EntityNotFoundException e) {
-        return new ApiError(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
+    public ResponseEntity<ApiError> entityNotExists(EntityNotFoundException e) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(new ApiError(status, e.getLocalizedMessage()), status);
     }
 }
